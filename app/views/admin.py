bd49d1c28205eca5ff7c0bcd6ded713ca2c6f7e4
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2015-11-12 23:17:37 +0530

    Basic admin module introduced

--- app/views/admin.py
+++ app/views/admin.py
@@ -1,12 +1,21 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from jinja2 import TemplateNotFound
 from flask.ext.login import login_required
 
 from app.auth import requires_roles
 
-mod = Blueprint('admin', __name__, )
+mod = Blueprint('admin', __name__)
 
 @mod.route('/', methods=["GET"])
 @login_required
-@requires_roles('admin')
+@requires_roles('user')
 def index():
-    return "Welcome Admin"
+    return render_template('admin/index.html')
+
+
+@mod.route('/<astro_id>/sync', methods=["POST"])
+@login_required
+@requires_roles('user')
+def sync_astro(astro_id):
+    ret = astrosService.sync(astro_id)
+    return jsonify(resp=ret)
--- app/views/astros.py
+++ app/views/astros.py
@@ -13,10 +13,3 @@ mod = Blueprint('astros', __name__, )
 def index():
     astros_meta = astrosService.get_astros_meta_info()
     return render_template('pages/astro_widget.html', astros=astros_meta)
-
-
-@mod.route('/<astro_id>/sync', methods=["POST"])
-@login_required
-def sync_astro(astro_id):
-    ret = astrosService.sync(astro_id)
-    return jsonify(resp=ret)
